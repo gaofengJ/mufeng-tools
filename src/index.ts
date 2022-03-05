d4bb2aa7e5ff12ca87a23e9fb130b54d0b1406be
gaofengJ <760613910@qq.com>
2022-03-05 15:49:35 +0800

    [feat]导出方法

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export default (name: string) => `Hello ${name}`;
+export * as dateFormat from './dateFormat';
+export * as dateGetAfterDay from './dateGetAfterDay';
+export * as dateGetBeforeDay from './dateGetBeforeDay';
+export * as getType from './getType';
