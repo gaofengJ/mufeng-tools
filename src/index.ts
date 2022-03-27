128d3cb2b82b647783155425ff713e5543ef0f64
gaofengJ <760613910@qq.com>
2022-03-27 16:02:16 +0800

    [feat]导出objectToArray和stringLineToHump方法

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import dateFormat from './dateFormat';
 import dateGetAfterDay from './dateGetAfterDay';
 import dateGetBeforeDay from './dateGetBeforeDay';
 import getType from './getType';
+import objectToArray from './objectToArray';
+import stringLineToHump from './stringLineToHump';
 
 export {
   dateFormat,
   dateGetAfterDay,
   dateGetBeforeDay,
   getType,
+  objectToArray,
+  stringLineToHump,
 };
