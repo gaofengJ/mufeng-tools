699c67510189951e77f3c0d3e0865cf32863bc04
木风 <>
2022-05-18 00:29:44 +0800

    [feat]版本更新

--- src/stringGetRandomStr.ts
+++ src/stringGetRandomStr.ts
@@ -1,7 +1,12 @@
+/**
+ * 生成随机字符串
+ * @param len 字符串长度
+ * @returns string
+ */
 export default function stringRandomStr(len: number = 32) {
   const t = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890-_';
   const charLen = t.length;
-  let n = '';
-  for (let i = 0; i < len; i += 1) n += t.charAt(Math.floor(Math.random() * charLen));
-  return n;
+  let str = '';
+  for (let i = 0; i < len; i += 1) str += t.charAt(Math.floor(Math.random() * charLen));
+  return str;
 }
